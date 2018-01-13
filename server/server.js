4e6d0a3df748221202e228c75c2d0b1099620567
Ashish Mishra <as06260@gmail.com>
2018-01-13 23:13:37 +0530

    Add patch  /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express=require('express');
-var bodyParser=require('body-parser');
+const _=require('lodash');
+const express=require('express');
+const bodyParser=require('body-parser');
 const {ObjectId}=require('mongodb');
 
 var {mongoose}=require('./db/mongoose');//destructuring
@@ -70,6 +71,33 @@ app.delete('/todos/:id',(req,res)=>{
     res.status(400).send(e);
   })
 
+});
+
+app.patch('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  var body=_.pick(req.body,['text','completed']);// pick takes the object and it pull off only usefull property if they exist
+
+  if(!ObjectId.isValid(id))
+  {
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed)&&body.completed){
+    body.completedAt=new Date().getTime();
+  }
+  else {
+    body.completed=false;
+    body.completedAt=null;
+  }
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+    if(!todo)
+    {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
 });
 app.listen(port,()=>{
   console.log(`started up at port ${port}`);
