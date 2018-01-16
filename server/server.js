a3ee53cda09df65a408c49bb52734122d6bf87b7
Ashish Mishra <as06260@gmail.com>
2018-01-16 23:28:58 +0530

    ADD delete users/me/token route

--- server/server.js
+++ server/server.js
@@ -103,9 +103,8 @@ app.patch('/todos/:id',(req,res)=>{
 });
 
 
-app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user);
-});
+
+
 
 
 app.post('/users',(req,res)=>{
@@ -125,6 +124,20 @@ user.save().then(()=>{
     res.status(400).send(e);
   });
 
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
+
+
+
+app.delete('/users/me/token',authenticate,(req,res)=>{
+  req.user.removeToken(req.token).then(()=>{
+    res.status(200).send();
+  },()=>{
+    res.status(400).send();
+  });
 });
 // app.post('/users', (req, res) => {
 //   var body = _.pick(req.body, ['email', 'password']);
